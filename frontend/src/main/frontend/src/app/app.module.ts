16a4cda813b8e9c2d8d68c66399f5413fa8e1aad
SarumanW <olgasergeevna0803@gmail.com>
2018-08-30 15:15:47 +0300

    Improved home component design, added login page.

--- frontend/src/main/frontend/src/app/app.module.ts
+++ frontend/src/main/frontend/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
 import {RoutingModule} from "./routing.module";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
